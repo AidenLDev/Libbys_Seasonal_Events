38e09bd1b66426f35318da41d90fbdac326718a0
github-is-garbage <github-is-garbage@users.noreply.github.com>
2024-10-31 02:25:31 -0400

    Add spinning and bobbing to collectables
    Also a debug renderer

--- lua/entities/libbys_event_collectable.lua
+++ lua/entities/libbys_event_collectable.lua
@@ -14,6 +14,12 @@ if CLIENT then
 	ENT.Instructions = "Touch it"
 
 	ENT.RenderGroup = RENDERGROUP_OPAQUE
+
+	AccessorFunc(ENT, "m_bRenderDebug", "RenderDebug", FORCE_BOOL)
+
+	AccessorFunc(ENT, "m_flBobAmount", "BobAmount", FORCE_NUMBER)
+	AccessorFunc(ENT, "m_bAllowNegativeBob", "AllowNegativeBob", FORCE_BOOL)
+	AccessorFunc(ENT, "m_flSpinSpeed", "SpinSpeed", FORCE_NUMBER)
 end
 
 if SERVER then
@@ -28,6 +34,12 @@ end
 function ENT:Initialize()
 	self:SelectModel()
 
+	if CLIENT then
+		self:SetBobAmount(10)
+		self:SetSpinSpeed(64)
+		self:SetAllowNegativeBob(false)
+	end
+
 	if SERVER then
 		self:SetTrigger(true)
 		self:SetUseType(SIMPLE_USE)
@@ -36,6 +48,63 @@ function ENT:Initialize()
 	self:SetCollisionGroup(COLLISION_GROUP_DEBRIS_TRIGGER)
 	self:SetMoveType(MOVETYPE_NONE)
 	self:SetSolid(SOLID_BBOX)
+end
+
+if CLIENT then
+	function ENT:Spin()
+		local SpinSpeed = self:GetSpinSpeed()
+		if SpinSpeed <= 0 then return end
+
+		local Spin = SpinSpeed * FrameTime()
+
+		local RenderAngle = self:GetRenderAngles() or self:EyeAngles()
+		RenderAngle.yaw = math.NormalizeAngle(RenderAngle.yaw + Spin)
+
+		self:SetRenderAngles(RenderAngle)
+	end
+
+	function ENT:Bob()
+		local BobAmount = self:GetBobAmount()
+		if BobAmount <= 0 then return end
+
+		local BobPercentage = math.sin(CurTime())
+		BobAmount = math.Remap(BobPercentage, -1, 1, -BobAmount, BobAmount)
+
+		if BobAmount < 0 and not self:GetAllowNegativeBob() then
+			BobAmount = -BobAmount
+		end
+
+		local RenderOrigin = self:GetNetworkOrigin()
+		RenderOrigin.z = RenderOrigin.z + BobAmount
+
+		self:SetRenderOrigin(RenderOrigin)
+	end
+
+	function ENT:Draw()
+		-- Debug visual of where it actually is
+		if self:GetRenderDebug() then
+			local RenderAngle = self:GetRenderAngles()
+			local RenderOrigin = self:GetRenderOrigin()
+
+			self:SetRenderAngles(angle_zero)
+			self:SetRenderOrigin(self:GetNetworkOrigin())
+			do
+				render.SetColorModulation(1, 0, 0)
+				do
+					self:DrawModel()
+					self:InvalidateBoneCache()
+				end
+				render.SetColorModulation(1, 1, 1)
+			end
+			self:SetRenderAngles(RenderAngle)
+			self:SetRenderOrigin(RenderOrigin)
+		end
+
+		self:Spin()
+		self:Bob()
+
+		self:DrawModel()
+	end
 end
 
 if SERVER then
