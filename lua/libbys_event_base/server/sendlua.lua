51ed8ab0261fa00aff07fbecd7b7fec07d424650
github-is-garbage <github-is-garbage@users.noreply.github.com>
2024-10-31 16:15:13 -0400

    Add BroadcastLongLua

--- lua/libbys_event_base/server/sendlua.lua
+++ lua/libbys_event_base/server/sendlua.lua
@@ -1,11 +1,17 @@
 util.AddNetworkString("SendLongLua")
 
--- SendLua but with 65535 character limit
-function LibbyEvent.SendLongLua(Player, Lua, ...)
+local function PrepareLua(Lua, ...)
 	Lua = Format(Lua, ...)
 
 	local Data = util.Compress(Lua)
 	local Size = string.len(Data)
+
+	return Data, Size
+end
+
+-- SendLua but with 65535 character limit
+function LibbyEvent.SendLongLua(Player, Lua, ...)
+	local Data, Size = PrepareLua(Lua, ...)
 
 	net.Start("SendLongLua")
 	do
@@ -13,4 +19,15 @@ function LibbyEvent.SendLongLua(Player, Lua, ...)
 		net.WriteData(Data, Size)
 	end
 	net.Send(Player)
+end
+
+function LibbyEvent.BroadcastLongLua(Lua, ...)
+	local Data, Size = PrepareLua(Lua, ...)
+
+	net.Start("SendLongLua")
+	do
+		net.WriteUInt(Size, 16)
+		net.WriteData(Data, Size)
+	end
+	net.Broadcast()
 end
