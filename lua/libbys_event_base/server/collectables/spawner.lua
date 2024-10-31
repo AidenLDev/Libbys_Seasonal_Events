099738c85d408da23b559e23e57b29f0d0955f22
github-is-garbage <github-is-garbage@users.noreply.github.com>
2024-10-31 04:17:24 -0400

    Revert spawner back to CreateAtRandom

--- lua/libbys_event_base/server/collectables/spawner.lua
+++ lua/libbys_event_base/server/collectables/spawner.lua
@@ -1,5 +1,5 @@
 timer.Create("LibbyEvent_CollectableSpawner", 1, 0, function()
-	local Collectable = LibbyEvent.Collectables.Create(Entity(1):GetPos() + Vector(0, 0, 150))
+	local Collectable = LibbyEvent.Collectables.CreateAtRandom()
 	if not IsValid(Collectable) then return end
 
 	Collectable:EmitSound("libbys/collectable_spawn.ogg")
