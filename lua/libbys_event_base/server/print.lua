c2a6fc6b46049f31975c5aa90c6aba00d75055e9
github-is-garbage <github-is-garbage@users.noreply.github.com>
2024-11-01 23:18:31 -0400

    Use ObjectToConstructorStr in PrintToPlayer
    Allows nice color printing from server to client. Works the same as MsgC (with a newline at the end)

--- lua/libbys_event_base/server/print.lua
+++ lua/libbys_event_base/server/print.lua
@@ -1,6 +1,16 @@
-function LibbyEvent.PrintToPlayer(Player, Message, ...)
-	Message = Format(Message, ...)
-	Message = string.EncapsulateOC(Message)
+function LibbyEvent.PrintToPlayer(Player, ...)
+	local ParameterCount = select("#", ...)
+	local Parameters = { ... }
 
-	LibbyEvent.SendLongLua(Player, [[MsgN(%s, "\n")]], Message)
+	for i = 1, ParameterCount do
+		local Converted, ConstructorStr = LibbyEvent.util.ObjectToConstructorStr(Parameters[i])
+
+		if not ConstructorStr then -- If we don't get a constructor it was tostring'd
+			Converted = string.EncapsulateOC(Parameters[i])
+		end
+
+		Parameters[i] = Converted
+	end
+
+	LibbyEvent.SendLongLua(Player, [[MsgC(%s, "\n")]], table.concat(Parameters, ", "))
 end
